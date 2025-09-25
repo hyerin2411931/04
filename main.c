0e24215fd86b4be42cb5539731a2645d37a91460
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-09-25 15:58:35 +0900

    Week03_04

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-  int second;
-  printf("input the second :");
-  scanf("%i", &second);
+  int year;
+  printf("input the year :");
+  scanf("%i", &year);
   
-  printf("the time is %i : %i\n", second/60, second%60); 
+  
+  printf("Is the year %i leap year?(yes=1, no=0) : %i\n", 
+  year,(year%4==0&&year%100 != 0)|| (year%400 == 0) );
+  
+  //(4로 나누어떨어지면서 100으론 안나누어떨어짐) 거나 (400으로 나누어떨어짐) 
   
   system("PAUSE");	
   return 0;
