ccc96e8d1ff99b9098e25a428c1caaf03bb642e8
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-09-25 16:07:13 +0900

    Week04_05

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-  int year;
-  printf("input the year :");
-  scanf("%i", &year);
+  //2진수로 표현했을때 1의 개수가 몇인가?
+  unsigned int x;  //음수 아니여야 해서 양수로 한정 
+  int b;
   
+  printf("input a number :");
+  scanf("%ui", &x);
   
-  printf("Is the year %i leap year?(yes=1, no=0) : %i\n", 
-  year,(year%4==0&&year%100 != 0)|| (year%400 == 0) );
+  for(b=0; x != 0; x >>= 1)
+  {
+           if (x & 1)   //x를 2진수로 표현했을 때 마지막 자릿수가 1인지 0인지 판별하는 식 
+           {
+                 b++;   //참이면 b를 하나 증가시켜라  
+           }
+  }
   
-  //(4로 나누어떨어지면서 100으론 안나누어떨어짐) 거나 (400으로 나누어떨어짐) 
-  
+  printf("The result is : %i\n", b);
   system("PAUSE");	
   return 0;
 }
