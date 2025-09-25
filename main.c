28218d0e7a20d5cba0974d8ec767cb966be8cbc6
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-09-25 16:13:48 +0900

    Week04_06

--- main.c
+++ main.c
@@ -3,22 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-  //2진수로 표현했을때 1의 개수가 몇인가?
-  unsigned int x;  //음수 아니여야 해서 양수로 한정 
-  int b;
-  
-  printf("input a number :");
-  scanf("%ui", &x);
-  
-  for(b=0; x != 0; x >>= 1)
-  {
-           if (x & 1)   //x를 2진수로 표현했을 때 마지막 자릿수가 1인지 0인지 판별하는 식 
-           {
-                 b++;   //참이면 b를 하나 증가시켜라  
-           }
-  }
-  
-  printf("The result is : %i\n", b);
+    int second;
+    printf("input the second: ");
+    scanf("%i", &second);
+    
+    printf("The time for %i second is %i : %i : %i\n",
+    second, second/3600, (second%3600)/60, (second%3600)%60); 
   system("PAUSE");	
   return 0;
 }
